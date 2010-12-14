6378cfad1452f8aa4a4fa2ba302da750a55c42b3
Kent Fredric <kentfredric@gmail.com>
2010-12-14 16:23:20 +1300

    Documentation time

--- lib/Paludis/UseCleaner.pm
+++ lib/Paludis/UseCleaner.pm
@@ -5,6 +5,28 @@ package Paludis::UseCleaner;
 
 # ABSTRACT: Remove cruft from your use.conf
 
+=head1 SYNOPSIS
+
+This module handles the core behaviour of the Use Cleaner, to be consumed inside other applications.
+
+For a "Just Use it" interface, you want L<paludis-usecleaner.pl> and L<Paludis::UseCleaner::App>
+
+    my $cleaner = Paludis::UseCleaner->new(
+        input     => somefd,
+        output    => somefd,
+        rejects   => somefd,
+        debug     => fd_for_debugging
+        dot_trace => fd_for_dot_traces,
+      ( # Optional
+        display_ui => $object_to_handle_debug_messages
+        display_ui_class => $classname_to_construct_a_display_ui
+        display_ui_generator => $coderef_to_generate_object_for_display_ui
+      )
+    );
+
+    $cleaner->do_work();
+=cut
+
 use Moose;
 use MooseX::Types::Moose qw( :all );
 use MooseX::Types::Perl qw( :all );
@@ -15,14 +37,88 @@ use Class::Load 0.06 qw( load_class );
 use Moose::Util::TypeConstraints qw( class_type );
 use MooseX::Has::Sugar;
 
+=attr input
+
+    $cleaner->input( \*STDIN );
+    $cleaner->input( $read_fh );
+
+
+=cut
+
 has 'input'                => ( isa => GlobRef,    rw, required );
+
+=attr output
+
+    $cleaner->output( \*STDOUT );
+    $cleaner->output( $write_fh );
+
+=cut
+
 has 'output'               => ( isa => GlobRef,    rw, required );
+
+=attr rejects
+
+    $cleaner->rejects( \*STDOUT );
+    $cleaner->rejects( $write_fh );
+
+=cut
+
 has 'rejects'              => ( isa => GlobRef,    rw, required );
+
+=attr debug
+
+    $cleaner->debug( \*STDERR );
+    $cleaner->debug( $write_fh );
+
+
+=cut
+
 has 'debug'                => ( isa => GlobRef,    rw, required );
+
+=attr dot_trace
+
+    $cleaner->dot_trace( \*STDERR );
+    $cleaner->dot_trace( $write_fh );
+
+=cut
+
 has 'dot_trace'            => ( isa => GlobRef,    rw, required );
+
+=attr display_ui
+
+    $cleaner->display_ui( $object );
+
+=cut
+
 has 'display_ui'           => ( isa => Object,     rw, lazy_build );
+
+=attr display_ui_class
+
+    $cleaner->display_ui_class( 'Some::Class::Name' );
+
+=cut
+
 has 'display_ui_class'     => ( isa => ModuleName, rw, lazy_build );
+
+=attr display_ui_generator
+
+    $cleaner->display_ui_generator( sub {
+        my $self = shift;
+        ....
+        return $object;
+    });
+
+=cut
+
 has 'display_ui_generator' => ( isa => CodeRef,    rw, lazy_build );
+
+=method do_work
+
+    $cleaner->do_work();
+
+Executes the various tranformations and produces the cleaned output from the input.
+
+=cut
 
 sub do_work {
 
@@ -65,39 +161,51 @@ sub do_work {
   }
 }
 
+=p_method __tokenize
+
+    my @line = __tokenize( $line );
+
+B<STRIPPED>: This method is made invisible to outside code after compile.
+
+=cut
+
 sub __tokenize {
   my $line = shift;
   $line =~ s/#.*$//;
   return split /\s+/, $line;
 }
 
+=p_method __is_empty_line
+
+    if( __is_empty_line(@line) ){ }
+
+B<STRIPPED>: This method is made invisible to outside code after compile.
+
+=cut
+
 sub __is_empty_line {
   return not @_;
 }
+
+=p_method __is_star_rule
+
+    if( __is_star_rule(@line) ){ }
+
+B<STRIPPED>: This method is made invisible to outside code after compile.
+
+=cut
 
 sub __is_star_rule {
   return $_[0] =~ /\*/;
 }
 
-sub _build_display_ui_class {
-  return 'Paludis::UseCleaner::ConsoleUI';
-}
+=p_method __tokenparse
 
-sub _build_display_ui_generator {
-  my $self = shift;
-  return sub {
-    load_class( $self->display_ui_class );
-    return $self->display_ui_class->new(
-      fd_debug     => $self->debug,
-      fd_dot_trace => $self->dot_trace,
-    );
-  };
-}
+    my ( $spec, $use, $extras ) = __tokenparse( @line );
 
-sub _build_display_ui {
-  my $self = shift;
-  return $self->display_ui_generator()->($self);
-}
+B<STRIPPED>: This method is made invisible to outside code after compile.
+
+=cut
 
 sub __tokenparse {
   my @tokens   = @_;
@@ -110,6 +218,15 @@ sub __tokenparse {
   return ( $spec, \@useflags, \%extras );
 }
 
+=p_method __extract_flags
+
+    my ( @flags ) = __extract_flags( \@tokens );
+
+
+B<STRIPPED>: This method is made invisible to outside code after compile.
+
+=cut
+
 sub __extract_flags {
   my $in = shift;
   my @out;
@@ -119,6 +236,14 @@ sub __extract_flags {
   return @out;
 }
 
+=p_method __extract_label
+
+    my ( $label ) = __extract_label( \@tokens );
+
+B<STRIPPED>: This method is made invisible to outside code after compile.
+
+=cut
+
 sub __extract_label {
   my $in = shift;
   return if not exists $in->[0];
@@ -126,6 +251,44 @@ sub __extract_label {
   my $result = $1;
   shift @$in;
   return $result;
+}
+
+=p_method _build_display_ui_class
+
+    my $class = $cleaner->_build_display_ui_class();
+
+=cut
+
+sub _build_display_ui_class {
+  return 'Paludis::UseCleaner::ConsoleUI';
+}
+
+=p_method _build_display_ui_generator
+
+    my $generator  $cleaner->_build_display_ui_generator();
+
+=cut
+
+sub _build_display_ui_generator {
+  my $self = shift;
+  return sub {
+    load_class( $self->display_ui_class );
+    return $self->display_ui_class->new(
+      fd_debug     => $self->debug,
+      fd_dot_trace => $self->dot_trace,
+    );
+  };
+}
+
+=p_method _build_display_ui
+
+    my $object = $cleaner->_build_display_ui();
+
+=cut
+
+sub _build_display_ui {
+  my $self = shift;
+  return $self->display_ui_generator()->($self);
 }
 
 1;
--- lib/Paludis/UseCleaner/ConsoleUI.pm
+++ lib/Paludis/UseCleaner/ConsoleUI.pm
@@ -12,15 +12,86 @@ use MooseX::Has::Sugar;
 use IO::Handle;
 use Term::ANSIColor;
 
+=attr show_skip_empty
+
+    $ui->show_skip_empty( 1 ); # enable showing the empty-line debug
+    $ui->show_skip_empty( 0 ); # disable ...
+
+B<default> is C<true>
+
+=cut
+
 has show_skip_empty => ( isa => Bool, rw, default => 1 );
+
+=attr show_skip_star
+
+    $ui->show_skip_star( 1 ); # enable showing the * rule debug
+    $ui->show_skip_star( 0 ); # disable ...
+
+B<default> is C<true>
+
+=cut
+
 has show_skip_star  => ( isa => Bool, rw, default => 1 );
+
+=attr show_dot_trace
+
+    $ui->show_dot_trace( 1 ); # enable showing the dot_trace's
+    $ui->show_dot_trace( 0 ); # disable ...
+
+B<default> is C<false>
+
+=cut
+
 has show_dot_trace  => ( isa => Bool, rw, default => 0 );
+
+=attr show_clean
+
+    $ui->show_clean( 1 ); # enable showing the clean notice
+    $ui->show_clean( 0 ); # disable ...
+
+B<default> is C<true>
+
+=cut
+
 has show_clean      => ( isa => Bool, rw, default => 1 );
+
+=attr show_rules
+
+    $ui->show_rules( 1 ); # enable showing the rule debug
+    $ui->show_rules( 0 ); # disable ...
+
+B<default> is C<true>
+
+=cut
+
 has show_rules      => ( isa => Bool, rw, default => 1 );
+
+=attr fd_debug
+
+    $ui->fd_debug( \*STDOUT ); # debug to stdout
+    $ui->fd_debug( $fh ); # debug to a filehandle
+
+=cut
+
 has fd_debug     => ( isa => GlobRef, rw, required );
+
+=attr fd_dot_trace
+
+    $ui->fd_dot_trace( \*STDOUT ); # debug to stdout
+    $ui->fd_dot_trace( $fh ); # debug to a filehandle
+
+=cut
+
 has fd_dot_trace => ( isa => GlobRef, rw, required );
 
 my $format = "%s%s\n >> %s\n >  %s%s\n";
+
+=p_method _message
+
+   ->_message( $colour, $label, $line , $reason )
+
+=cut
 
 sub _message {
   my ( $self, $colour, $label, $line, $reason ) = @_;
@@ -29,12 +100,32 @@ sub _message {
 
 }
 
+=method skip_empty
+
+    $ui->skip_empty( $lineno, $line )
+
+Notifies user a line has been skipped in the input due to it being empty.
+This line has been copied to the output ( cleaned ) file.
+
+=cut
+
 sub skip_empty {
   my ( $self, $lineno, $line ) = @_;
   $self->dot_trace('>');
   return unless $self->show_skip_empty;
   $self->_message( 'red', "Skipping $lineno", $line, "Looks empty" );
 }
+
+=method skip_star
+
+    $ui->skip_star( $lineno, $line )
+
+Notifies user a line has been skipped in the input due to it being a * rule,
+and thus having far too many possible matches to compute respectably.
+
+This line has been copied to the output ( cleaned ) file.
+
+=cut
 
 sub skip_star {
   my ( $self, $lineno, $line ) = @_;
@@ -43,12 +134,31 @@ sub skip_star {
   $self->_message( 'red', "Skipping $lineno", $line, "* rule" );
 }
 
+=method dot_trace
+
+    $ui->dot_trace( $symbol = '.' )
+
+Prints a simple progress indicator when show_dot_trace is enabled.
+
+=cut
+
 sub dot_trace {
   my ( $self, $symbol ) = @_;
   return unless $self->show_dot_trace;
   $symbol ||= '.';
   $self->fd_dot_trace->print($symbol);
 }
+
+=method nomatch
+
+    $ui->nomatch( $lineno, $line )
+
+Notifies use that a line appeared to contain a rule and that rule matched
+no packages that exist, both uninstalled and installed, and is thus being removed from the output( cleaned ) file.
+
+Just In case, this line is also copied to the rejects file.
+
+=cut
 
 sub nomatch {
   my ( $self, $lineno, $line ) = @_;
@@ -57,6 +167,15 @@ sub nomatch {
   $self->_message( 'green', "Cleaning $lineno", $line, 'No matching specification' );
 }
 
+=method full_rule
+
+    $extrasmap{VIDEO_CARDS} = \@cardlist;
+
+    $ui->full_rule( $spec, \@useflags, \%extrasmap )
+
+Prduces a debug tracing line showing the parsed result of the line as we perceive it internally.
+
+=cut
 sub full_rule {
   my ( $self, $spec, $use, $extras ) = @_;
   return unless $self->show_rules;
